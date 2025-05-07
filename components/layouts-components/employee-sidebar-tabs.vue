4399f093d79dac30406fe5739d20e54a397a5190
AMRWERY <amrmounir2@gmail.com>
2025-05-07 18:26:10 +0300

    some layout enhancments

--- components/layouts-components/employee-sidebar-tabs.vue
+++ components/layouts-components/employee-sidebar-tabs.vue
@@ -1,52 +1,57 @@
 <template>
   <div>
-    <li v-if="hasAnyRole('employee')">
-      <nuxt-link to="" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-800 group">
-        <icon name="material-symbols:dashboard"
-          class="w-5 h-5 text-gray-400 transition duration-75 group-hover:text-white" aria-hidden="true" />
-        <span class="flex-1 ms-3 whitespace-nowrap">{{ $t('layouts.dashboard') }}</span>
-      </nuxt-link>
-    </li>
+    <div class="space-y-3">
+      <li v-if="hasAnyRole('employee')">
+        <nuxt-link-locale to="/" class="flex items-center p-2 text-white rounded-lg group"
+          exact-active-class="bg-gray-400 text-white hover:bg-gray-500">
+          <icon name="material-symbols:dashboard" class="w-5 h-5 transition duration-75 group-hover:text-white"
+            :class="{ 'text-gray-400': $route.path === '/', 'text-white': $route.path !== '/' }" aria-hidden="true" />
+          <span class="flex-1 ms-3 whitespace-nowrap">{{ $t('layouts.dashboard') }}</span>
+        </nuxt-link-locale>
+      </li>
 
-    <li v-if="hasAnyRole('employee')">
-      <button @click="toggleDropdown('personal')" type="button"
-        class="flex items-center w-full p-2 text-base text-white transition duration-75 rounded-lg group hover:bg-gray-800">
-        <icon name="icon-park-solid:personal-privacy"
-          class="flex-shrink-0 w-5 h-5 text-gray-400 transition duration-75 group-hover:text-white"
-          aria-hidden="true" />
-        <span class="flex-1 ms-3 text-start whitespace-nowrap">{{ $t('layouts.personal') }}</span>
-        <icon name="material-symbols:keyboard-arrow-down-rounded" class="w-5 h-5"
-          :class="{ 'rotate-180': openDropdowns.personal }" aria-hidden="true" />
-      </button>
-      <ul v-if="openDropdowns.personal" class="py-2 space-y-2">
-        <li>
-          <nuxt-link-locale to="/employee/my-attendance"
-            class="flex items-center w-full p-2 text-white transition duration-75 rounded-lg ps-14 group hover:bg-gray-800">
-            {{ $t('layouts.my_attendance') }}
-          </nuxt-link-locale>
-        </li>
-        <li>
-          <nuxt-link to=""
-            class="flex items-center w-full p-2 text-white transition duration-75 rounded-lg ps-14 group hover:bg-gray-800">
-            {{ $t('layouts.my_leave_requests') }}
-          </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to=""
-            class="flex items-center w-full p-2 text-white transition duration-75 rounded-lg ps-14 group hover:bg-gray-800">
-            {{ $t('layouts.my_performance_reviews') }}
-          </nuxt-link>
-        </li>
-      </ul>
-    </li>
+      <li v-if="hasAnyRole('employee')">
+        <button @click="toggleDropdown('personal')" type="button"
+          class="flex items-center w-full p-2 text-base text-white transition duration-75 rounded-lg group hover:bg-gray-800">
+          <icon name="icon-park-solid:personal-privacy"
+            class="flex-shrink-0 w-5 h-5 text-gray-400 transition duration-75 group-hover:text-white"
+            aria-hidden="true" />
+          <span class="flex-1 ms-3 text-start whitespace-nowrap">{{ $t('layouts.personal') }}</span>
+          <icon name="material-symbols:keyboard-arrow-down-rounded" class="w-5 h-5"
+            :class="{ 'rotate-180': openDropdowns.personal }" aria-hidden="true" />
+        </button>
+        <ul v-if="openDropdowns.personal" class="py-2 space-y-2">
+          <li>
+            <nuxt-link-locale to="/employee/my-attendance"
+              class="flex items-center p-2 text-white rounded-lg group ps-14"
+              exact-active-class="bg-gray-400 text-white hover:bg-gray-500">
+              {{ $t('layouts.my_attendance') }}
+            </nuxt-link-locale>
+          </li>
+          <li>
+            <nuxt-link to="" class="flex items-center p-2 text-white rounded-lg group ps-14"
+              exact-active-class="bg-gray-400 text-white hover:bg-gray-500">
+              {{ $t('layouts.my_leave_requests') }}
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="" class="flex items-center p-2 text-white rounded-lg group ps-14"
+              exact-active-class="bg-gray-400 text-white hover:bg-gray-500">
+              {{ $t('layouts.my_performance_reviews') }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </li>
 
-    <li v-if="hasAnyRole('employee')">
-      <nuxt-link to="" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-800 group">
-        <icon name="solar:hand-money-linear" class="w-5 h-5 text-gray-400 transition duration-75 group-hover:text-white"
-          aria-hidden="true" />
-        <span class="flex-1 ms-3 whitespace-nowrap">{{ $t('layouts.my_payslips_payroll') }}</span>
-      </nuxt-link>
-    </li>
+      <li v-if="hasAnyRole('employee')">
+        <nuxt-link to="" class="flex items-center p-2 text-white rounded-lg group"
+          exact-active-class="bg-gray-400 text-white hover:bg-gray-500">
+          <icon name="solar:hand-money-linear" class="w-5 h-5 transition duration-75 group-hover:text-white"
+            :class="{ 'text-gray-400': $route.path === '', 'text-white': $route.path !== '' }" aria-hidden="true" />
+          <span class="flex-1 ms-3 whitespace-nowrap">{{ $t('layouts.my_payslips_payroll') }}</span>
+        </nuxt-link>
+      </li>
+    </div>
   </div>
 </template>
 
